2dfcade379fba85db0106e11a359efd3271473fa
Francisco J <franlsym@users.noreply.github.com>
2025-03-10 15:48:19 +0000

    add header and footer loader script

---
+++ src/js/header-footer-loader.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const components = {
+        "lheader": "/components/logged-header.html",
+        "uheader": "/components/unlogged-header.html",
+        "footer": "/components/footer.html",
+    };
+
+    Object.keys(components).forEach(id => {
+        const element = document.querySelector(id);
+        if (element) {
+            fetch(components[id])
+                .then(response => response.text())
+                .then(data => {
+                    element.outerHTML = data;
+                })
+                .catch(error => console.error(`Error cargando el ${id}:`, error));
+        }
+    });
+});
